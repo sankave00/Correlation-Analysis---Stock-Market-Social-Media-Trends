6c0a3a0b51c9ea84f160efc30b5bd5953bc78a2b
Sanyog Kave P <kavesanyog@gmail.com>
2023-02-05 13:59:20 +0530

    Twitter scripts and dataset correction

--- Datasets/ApiScripts/TwitterApi.py
+++ Datasets/ApiScripts/TwitterApi.py
@@ -37,14 +37,6 @@ def get_tweet_data(query_params, page_count = 10):
             for tweet in json_response['data']:
                 if tweet['lang'] == 'en':
                     extracted_tweets.append(tweet['text'])
-        
-        if 'meta' in json_response:
-            if 'next_token' in json_response['meta']:
-                query_params['next_token'] = json_response['meta']['next_token']
-                response = requests.request("GET", url, auth=bearer_oauth, params=query_params)
-                if response.status_code != 200:
-                    raise Exception(response.status_code, response.text)
-                json_response = response.json()
     
     print(f'\t\t *** Extracted {len(extracted_tweets)} tweets ***')
     return extracted_tweets
@@ -94,9 +86,9 @@ if __name__ == "__main__":
     }
     
     
-    next_date = datetime.datetime(2023, 1, 30)
+    next_date = datetime.datetime(2023, 2, 3)
     
-    end_date = datetime.datetime(2023, 2, 1)
+    end_date = datetime.datetime(2023, 2, 5)
     
     query_params = {}
     query_params['tweet.fields'] = 'created_at,lang,source'
@@ -105,6 +97,7 @@ if __name__ == "__main__":
         
         query_params['start_time'] = f'{next_date.strftime("%Y-%m-%d")}T00:00:00Z'
         query_params['end_time'] = f'{next_date.strftime("%Y-%m-%d")}T23:59:59Z'
+        print(query_params)
 
         print(f"Fetching data for {next_date.strftime('%Y-%m-%d')}")
         
