0aa567b156ae7e6296e43b188f09d865cd8965dc
sankave00 <kavesanyog@gmail.com>
2023-04-22 13:18:13 +0530

    Prediction with results

---
+++ MainScript.py
@@ -0,0 +1,15 @@
+twitter_keywords = {
+    "Finance": ["Morgan Stanley", "investment","#MS", "$MS" ],
+    "EVs": ["tesla","EVs","#TSLA", "$TSLA"],
+    "Tech": ["Google","#GOOGL","#GOOG","$GOOGL","$GOOG"]
+}
+reddit_keywords = {
+    "Finance": ["'Morgan Stanley'","'stock price'", "investment","MS" ],
+    "EVs": ["tesla","EVs","'electric vehicle'","TSLA"],
+    "Tech": ["Google","'Alphabet Inc.'","GOOGL"]
+}
+ticker_list = {
+    "Finance": ["MS"],
+    "EVs": ["TSLA"],
+    "Tech": ["GOOGL"]   
+}
